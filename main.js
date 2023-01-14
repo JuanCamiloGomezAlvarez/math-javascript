8b82beea30020f7841ab918f26bd2cf5597d0904
JuanCamiloGomezAlvarez <kamygz93@hotmail.com>
2023-01-14 17:14:51 -0500

    dando orden al codigo

--- main.js
+++ main.js
@@ -1,28 +1,63 @@
 import './style.css'
 
-
-
 document.querySelector('#app').innerHTML = `
   <main>
     <h1 >Matemáticas con JavaScript!</h1>
     <div  id="cardContainer" class="card-container">
+        <div class="triangulos card">
+          <h3>Calcula la altura de un triangulo isosceles</h3>
+          <form id="formTrianguloIsosceles">
+              <input id="ladosTriangulo" type="text" required placeholder="cual es el lado que se repite">
+              <input id="baseTriangulo" type="text" required placeholder="cual es el valor de la base">
+              <button id="btn">calcular</button>
+              <p id="resultTrianguloIsoseles">resultado</p>
+          </form> 
+        </div>
+        <div class="rectangulosPerimetro card">
+          <h3>Calcula el perimetro de un rectangulo</h3>
+          <form id="formRectangulo">
+            <input id="ladoCorto" type="text" required placeholder="cual es el lado mas corto" >
+            <input id="ladoLargo" type="text" required placeholder="cual es el lado mas largo">
+            <button id="btn">calcular</button>
+            <p id="resultRectanguloPerimetro">resultado</p>
+          </form>
+        </div>
+        <div class="rectangulosArea card">
+          <h3>Calcula el area de un rectangulo</h3>
+          <form id="formRectanguloArea">
+              <input id="lado1Rectangulo" type="text" required placeholder="cual es el lado mas corto">
+              <input id="lado2Rectangulo" type="text" required placeholder="cual es el lado mas largo">
+              <button id="btn">calcular</button>
+              <p id="resultRectanguloArea">resultado</p>
+          </form>
+        </div>
+        <div class="areaCirculo card">
+          <h3>Calcula el area de un circulo</h3>
+          <form id="formCirculoArea">
+              <input id="radioCirculo" type="text" required placeholder="cual es el valor del radio">
+              <button id="btn">calcular</button>
+              <p id="resultCirculoArea">resultado</p>
+          </form>
+        </div>
+        <div class="calculoDescuento card">
+          <h3>Calcula el descuento de un producto</h3>
+          <form id="formDescuento">
+              <input id="valorProducto" type="text" required placeholder="cual es el valor del producto">
+              <input id="porcentajeDescuento" type="text" required placeholder="porcentaje de descuento">
+              <br>
+              <label for="cupon" >agrega el nombre de tu cupon de descuento
+              <br>
+              <input id="cupon" type="text" placeholder="ej: jcga">
+              <button id="btn">calcular</button>
+              <p id="resultPorcentaje">resultado</p>
+          </form>
+        </div>
     </div>
   </main>
 `
 //------------------------------------------------------------------------------
 //ESTA SECCION ME AYUDA A CALCULAR LA ALTURA DE UN TRIANGULO ISOSCELES
-document.querySelector('#cardContainer').innerHTML += `
-  
-  <div class="triangulos card">
-    <h3>Calcula la altura de un triangulo isosceles</h3>
-    <form id="formTrianguloIsosceles">
-        <input id="ladosTriangulo" type="text" required placeholder="cual es el lado que se repite">
-        <input id="baseTriangulo" type="text" required placeholder="cual es el valor de la base">
-        <button id="btn">calcular</button>
-        <p id="resultTrianguloIsoseles">resultado</p>
-    </form> 
-  </div>
-`
+
 const resultTrianguloIsoseles = document.querySelector("#resultTrianguloIsoseles")
 const ladosTriangulo = document.querySelector("#ladosTriangulo")
 const baseTriangulo = document.querySelector("#baseTriangulo")
@@ -45,18 +80,7 @@ formTrianguloIsosceles.addEventListener("submit", calcularOnClickIsosceles)
 //-------------------------------------------------------------------------
 
 //ESTA SECCION ME AYUDA A CALCULAR EL PERIMETRO DE UN RECTANGULO
-document.querySelector('#cardContainer').innerHTML += `
-  
-  <div class="rectangulosPerimetro card">
-    <h3>Calcula el perimetro de un rectangulo</h3>
-    <form id="formRectangulo">
-      <input id="ladoCorto" type="text" required placeholder="cual es el lado mas corto" >
-      <input id="ladoLargo" type="text" required placeholder="cual es el lado mas largo">
-      <button id="btn">calcular</button>
-      <p id="resultRectanguloPerimetro">resultado</p>
-    </form>
-  </div>
-`
+
 const ladoCorto = document.querySelector("#ladoCorto")
 const ladoLargo = document.querySelector("#ladoLargo")
 const resultRectanguloPerimetro = document.querySelector("#resultRectanguloPerimetro")
@@ -78,18 +102,7 @@ formRectangulo.addEventListener("submit", calcularOnClickPerimetroRectangulo)
 //-------------------------------------------------------------------------
 
 //ESTA SECCION ME AYUDA A CALCULAR EL AREA DE UN RECTANGULO
-document.querySelector('#cardContainer').innerHTML += `
-  
-  <div class="rectangulosArea card">
-    <h3>Calcula el area de un rectangulo</h3>
-    <form id="formRectanguloArea">
-        <input id="lado1Rectangulo" type="text" required placeholder="cual es el lado mas corto">
-        <input id="lado2Rectangulo" type="text" required placeholder="cual es el lado mas largo">
-        <button id="btn">calcular</button>
-        <p id="resultRectanguloArea">resultado</p>
-    </form>
-  </div>
-`
+
 const lado1Rectangulo = document.querySelector("#lado1Rectangulo")
 const lado2Rectangulo = document.querySelector("#lado2Rectangulo")
 const resultRectanguloArea = document.querySelector("#resultRectanguloArea")
@@ -110,17 +123,6 @@ formRectanguloArea.addEventListener("submit", calcularOnClickAreaRectangulo)
 //-------------------------------------------------------------------------
 
 //ESTA SECCION PERMITE CALCULAR EL AREA DE UN CIRCULO DADO EL VALOR DEL RADIO
-document.querySelector('#cardContainer').innerHTML += `
-  
-  <div class="areaCirculo card">
-    <h3>Calcula el area de un circulo</h3>
-    <form id="formCirculoArea">
-        <input id="radioCirculo" type="text" required placeholder="cual es el valor del radio">
-        <button id="btn">calcular</button>
-        <p id="resultCirculoArea">resultado</p>
-    </form>
-  </div>
-`
 
 const radioCirculo = document.querySelector("#radioCirculo")
 const formCirculoArea = document.querySelector("#formCirculoArea")
@@ -133,7 +135,9 @@ function areaCirculo(radio){
 function calcularOnClickAreaCirculo(e){
   e.preventDefault()
   const radio = parseInt(radioCirculo.value)
+  console.log(radio)
   const area = areaCirculo(radio)
+  console.log(area)
   resultCirculoArea.innerHTML = area 
 }
 formCirculoArea.addEventListener("submit", calcularOnClickAreaCirculo)
@@ -141,22 +145,7 @@ formCirculoArea.addEventListener("submit", calcularOnClickAreaCirculo)
 //---------------------------------------------------------------------
 
 //ESTA SECCION PERMITE CALCULAR EL DESCUENTO DE UN PRODUCTO
-document.querySelector('#cardContainer').innerHTML += `
-  
-  <div class="calculoDescuento card">
-    <h3>Calcula el descuento de un producto</h3>
-    <form id="formDescuento">
-        <input id="valorProducto" type="text" required placeholder="cual es el valor del producto">
-        <input id="porcentajeDescuento" type="text" required placeholder="porcentaje de descuento">
-        <br>
-        <label for="cupon" >agrega el nombre de tu cupon de descuento
-        <br>
-        <input id="cupon" type="text" placeholder="ej: jcga">
-        <button id="btn">calcular</button>
-        <p id="resultPorcentaje">resultado</p>
-    </form>
-  </div>
-`
+
 const formDescuento = document.querySelector("#formDescuento")
 const valorProducto = document.querySelector("#valorProducto")
 const porcentajeDescuento = document.querySelector("#porcentajeDescuento")
@@ -206,18 +195,6 @@ function porcentajeFinal(valorProducto, porcentajeDescuento, cupon){
     }else{
       return "Cupon no valido, intente de nuevo"
     }
-  
-    // if(cupon === "jcga"){
-    //   const resultado1 = resultado - 10000 
-    //   return `El valor final del producto con descuento y cupon jcga es ${resultado1.toFixed(2)}`
-    // }else if(cupon === "bgpg"){
-    //   const resultado1 = resultado - 20000 
-    //   return `El valor final del producto con descuento y cupon bgpg es ${resultado1.toFixed(2)}`
-    // }else if(cupon === ""){
-    //   return `El valor final del producto con descuento es ${resultado.toFixed(2)}`
-    // }else{
-    //   return `Cupon no valido, el valor final del producto con descuento es ${resultado.toFixed(2)}`
-    // }
   } 
 }
 
